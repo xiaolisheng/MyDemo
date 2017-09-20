9c0fa7c4613d90a43d7208b97e9ac85a5c0a29aa
Lisen <450890134@qq.com>
2017-09-20 18:06:34 +0800

    123

---
+++ src/main/java/indi/lisen/dao/UsersMapper.java
@@ -0,0 +1,22 @@
+package indi.lisen.dao;
+
+import indi.lisen.model.Users;
+import indi.lisen.model.UsersExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface UsersMapper {
+    long countByExample(UsersExample example);
+
+    int deleteByExample(UsersExample example);
+
+    int insert(Users record);
+
+    int insertSelective(Users record);
+
+    List<Users> selectByExample(UsersExample example);
+
+    int updateByExampleSelective(@Param("record") Users record, @Param("example") UsersExample example);
+
+    int updateByExample(@Param("record") Users record, @Param("example") UsersExample example);
+}
